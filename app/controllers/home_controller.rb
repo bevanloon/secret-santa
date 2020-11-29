b27c3b74db57dcff2e06e381c086023b1a4143c1
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2020-11-29 12:55:55 +0000

    Add wishlist routes and actions to save
    Update db schema.
    
    Add the routes for displaying and saving the wishlist.
    
    Add the wishlist view.
    
    Add the wishlist display and save actions in the home controller.
    
    The app is small enough that we don't really need another controller
    yet, though arguably, we should split this out eventually.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def wishlist
+  end
+
+  def save_wishlist
+    permitted = params.permit(:wishlist)
+    wishlist = permitted[:wishlist].presence
+    if wishlist
+      current_user.update(wishlist: wishlist)
+      flash[:notice] = "Wishlist saved"
+      redirect_to root_url
+    else
+      if current_user.wishlist.presence
+        flash[:old_wishlist] = current_user.wishlist
+      end
+      current_user.update(wishlist: "")
+      flash[:alert] = "You've chosen to leave your wishlist blank.<br>If you save something in your wishlist, it will help your secret santa find something you'll appreciate."
+      render "wishlist"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+  get "/wishlist", to: "home#wishlist"
+  post "/wishlist", to: "home#save_wishlist"
 end
