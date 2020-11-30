b61b2c32819c1165086b8f0e7a2e5c710e8dd481
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2020-11-30 09:32:25 +0000

    Update app config and tidy spec_helper
    Update app config to GOV.UK defaults and merge rails and spec helper

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module GovukSecretSanta
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # GOV.UK convention is to use lib over app/lib
+    config.autoload_paths << "lib"
+    # GOV.UK convention is to use London time (but not for ActiveRecord)
+    config.time_zone = "London"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
